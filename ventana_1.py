b1310d790d1a1ccedb9a1f58d1f7c0135fde867f
Josix09 <jorgesilva@colegioguanenta.edu.co>
2025-07-31 15:45:39 -0500

    UP

---
+++ ventana_1.py
@@ -0,0 +1,26 @@
+# se importa la libreria tkinter con todas sus funciones
+from tkinter import *
+
+#------------------------------
+# Funciones de la app
+#------------------------------
+
+
+#------------------------------
+# Ventana principal de la app
+#------------------------------
+
+# se declara una variable llamada ventana principal que adquiere las características de un objeto tipo Tk()
+
+Ventana_principal = Tk()
+
+#Titulo de la ventana
+
+Ventana_principal.title("Jorge Luis Silva Morales")
+
+#tamaño de la ventana
+Ventana_principal.geometry("800x500")
+
+#metodo principal que despliega la ventana en pantalla
+Ventana_principal.mainloop()
+
