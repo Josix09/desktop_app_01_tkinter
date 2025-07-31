b0ed489090eea6150e80a7f1934da4689310c3ae
Josix09 <jorgesilva@colegioguanenta.edu.co>
2025-07-31 16:46:46 -0500

    UP

--- ventana_1.py
+++ ventana_1.py
@@ -21,6 +21,13 @@ Ventana_principal.title("Jorge Luis Silva Morales")
 #tamaño de la ventana
 Ventana_principal.geometry("800x500")
 
+#Deshabilitar el botón de maximizar de la ventana
+Ventana_principal.resizable(0,0)
+
+#color de fondo de la ventana
+
+Ventana_principal.config(bg="green")
+
 #metodo principal que despliega la ventana en pantalla
 Ventana_principal.mainloop()
 
