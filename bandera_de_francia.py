3dfa9281ae3d9825d7d52013fc7fa691d93f511f
Josix09 <jorgesilva@colegioguanenta.edu.co>
2025-08-01 08:19:15 -0500

    up

--- bandera_de_francia.py
+++ bandera_de_francia.py
@@ -26,29 +26,28 @@ Ventana_principal.resizable(0,0)
 
 #color de fondo de la ventana
 
-Ventana_principal.config(bg="green")
+Ventana_principal.config(bg="black")
 
 #-------------------------------
 #Frame_1
 #-------------------------------
 frame_1 = Frame(Ventana_principal)
-frame_1.config(bg="yellow",width=780, height=250)
+frame_1.config(bg="blue",width=280, height=480)
 frame_1.place(x=10,y=10)
 #-------------------------------
 #Frame 2
 #-------------------------------
 frame_2 = Frame(Ventana_principal)
-frame_2.config(bg="blue",width=780, height=120)
-frame_2.place(x=10,y=250)
+frame_2.config(bg="white",width=300, height=480)
+frame_2.place(x=280,y=10)
 #-------------------------------
 #Bandera
 #-------------------------------
 frame_3 = Frame(Ventana_principal)
-frame_3.config(bg="red",width=780, height=120)
-frame_3.place(x=10,y=370)
+frame_3.config(bg="red",width=240, height=480)
+frame_3.place(x=550,y=10)
 
 
 
 #metodo principal que despliega la ventana en pantalla
 Ventana_principal.mainloop()
-
--- bandera_noruega.py
+++ bandera_noruega.py
@@ -26,29 +26,36 @@ Ventana_principal.resizable(0,0)
 
 #color de fondo de la ventana
 
-Ventana_principal.config(bg="green")
+Ventana_principal.config(bg="black")
 
 #-------------------------------
 #Frame_1
 #-------------------------------
 frame_1 = Frame(Ventana_principal)
-frame_1.config(bg="yellow",width=780, height=250)
+frame_1.config(bg="dark red",width=780, height=480)
 frame_1.place(x=10,y=10)
 #-------------------------------
 #Frame 2
 #-------------------------------
 frame_2 = Frame(Ventana_principal)
-frame_2.config(bg="blue",width=780, height=120)
-frame_2.place(x=10,y=250)
+frame_2.config(bg="white",width=100, height=480)
+frame_2.place(x=160,y=10)
+
+frame_3 = Frame(Ventana_principal)
+frame_3.config(bg="white",width=780, height=100)
+frame_3.place(x=10,y=200)
 #-------------------------------
 #Bandera
 #-------------------------------
-frame_3 = Frame(Ventana_principal)
-frame_3.config(bg="red",width=780, height=120)
-frame_3.place(x=10,y=370)
+frame_4 = Frame(Ventana_principal)
+frame_4.config(bg="dark blue",width=780, height=60)
+frame_4.place(x=10,y=220)
+
+frame_5 = Frame(Ventana_principal)
+frame_5.config(bg="dark blue",width=60, height=480)
+frame_5.place(x=180,y=10)
 
 
 
 #metodo principal que despliega la ventana en pantalla
 Ventana_principal.mainloop()
-
